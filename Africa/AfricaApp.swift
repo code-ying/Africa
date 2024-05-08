a0363b2cec11fba0819e4d82c2c2542e43909002
raihan <ramadianraihan@gmail.com>
2024-05-08 18:12:46 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Muhammad Raihan Rahmadian on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
